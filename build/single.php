7dde46e0cffbac9bb853f4ac3f8726391948db1a
bahrynowskijakub <bahrynowski.praca@gmail.com>
2023-11-08 08:08:14 +0100

    default template

--- build/single.php
+++ build/single.php
@@ -1,6 +1,7 @@
-<?php get_header();
+<?php get_header(); ?>
 
-if(in_category("przepisy")) include "partials/post-types/recipe.php";
-else include "partials/post-types/article.php";
+<section class="section">
+    <? the_content(); ?>
+</section>
 
-get_footer(); ?>
+<? get_footer(); ?>
